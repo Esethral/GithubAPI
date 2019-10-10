5b2df3fb3bad4508bf1aa7d4c2dc64b169dc30fb
Esethral <eabel@stevens.edu>
2019-10-10 17:20:03 -0400

    Add files via upload

--- HW4_Test.py
+++ HW4_Test.py
@@ -6,9 +6,9 @@ from HW4 import getRequest
 class TestAPI(unittest.TestCase):
 
 	def API_test(self):
-		self.assertEqual(getRequest('Esethral', 5),"")
-		self.assertEqual(getRequest('Esethral', 4),"Repo: TriangleTesting || Commits: 5\n")
-		self.assertEqual(getRequest('Esethral', 3),"Repo: SSW-567-WS-Testing || Commits: 4\nRepo: TriangleTesting || Commits: 5\n")
+		self.assertEqual(getRequest('esethral', 5),"", "Invalid")
+		self.assertEqual(getRequest('esethral', 4),"Repo: TriangleTesting || Commits: 5\n", "Invalid")
+		self.assertEqual(getRequest('esethral', 3),"Repo: SSW-567-WS-Testing || Commits: 4\nRepo: TriangleTesting || Commits: 5\n", "Invalid")
 
 if __name__ == '__main__':
     print('Running unit tests')
